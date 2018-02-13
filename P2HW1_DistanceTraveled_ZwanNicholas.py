8dac4a94af2e04219fa226ba112301674fa23c3e
nzwan <36454311+nzwan@users.noreply.github.com>
2018-02-13 18:47:39 +0000

    P2HW1
    Distance traveled P2HW1

---
+++ P2HW1_DistanceTraveled_ZwanNicholas.py
@@ -0,0 +1,10 @@
+# CTI-110
+# P2HW1
+# Nicholas A. Zwan
+# 02/13/18
+# Calculate distance of vehicle traveling at multiple speeds over various time periods.
+
+speed = ('70 mph')
+distanceAfter6 = ('420 miles')
+distanceAfter10 = ('700 miles')
+distanceAfter15 = ('1050 miles')
